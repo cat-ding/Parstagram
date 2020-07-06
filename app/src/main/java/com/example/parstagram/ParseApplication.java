d633605b7d55003a9f369a4dbf5e926ec93e7ada
cat-ding <catherineding28@gmail.com>
2020-07-06 17:08:13 -0400

    finished ability to login using Parse

---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    public static final String APP_ID = "cat-parstagram";
+    public static final String CLIENT_KEY = "CodepathCatParstagramMaster";
+    public static final String SERVER_URL = "https://cat-parstagram.herokuapp.com/parse";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(APP_ID) // should correspond to APP_ID env variable
+                .clientKey(CLIENT_KEY)  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server(SERVER_URL).build());
+    }
+}
